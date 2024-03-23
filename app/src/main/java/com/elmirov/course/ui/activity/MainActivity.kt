84ac520e6f0b770bc0634e03686faed04d1db5ec
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-03-23 15:51:18 +0700

    Поменял activity

--- app/src/main/java/com/elmirov/course/ui/activity/MainActivity.kt
+++ app/src/main/java/com/elmirov/course/ui/activity/MainActivity.kt
@@ -1,37 +1,17 @@
 package com.elmirov.course.ui.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.elmirov.course.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val emojis = intArrayOf(
-        0x1f600,
-        0x1f603,
-        0x1f604,
-        0x1f601,
-        0x1f606,
-        0x1f605,
-        0x1f923,
-        0x1f602,
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.fragmentContainer) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
