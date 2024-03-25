203ee45ef59e95acbc0cb4f03ec1628416498111
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-03-25 14:26:15 +0700

    Обновил сущность сообщения

--- app/src/main/java/com/elmirov/course/domain/Message.kt
+++ app/src/main/java/com/elmirov/course/domain/Message.kt
@@ -1,7 +1,12 @@
 package com.elmirov.course.domain
 
+import java.util.UUID
+
 data class Message(
+    val id: UUID = UUID.randomUUID(),
+    val userId: Int,
+    val data: String? = null,
     val authorName: String,
     val text: String,
-    val reactions: Map<Int, Int>
+    val reactions: Map<Int, Int>? = null
 )
