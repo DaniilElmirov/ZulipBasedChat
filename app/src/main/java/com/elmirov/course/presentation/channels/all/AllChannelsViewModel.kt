fb5c51da7a198b8bad379286f55746eefbfb1e13
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-02 12:35:43 +0700

    Поменял логику во вью моделях экранов с channel

--- app/src/main/java/com/elmirov/course/presentation/channels/all/AllChannelsViewModel.kt
+++ app/src/main/java/com/elmirov/course/presentation/channels/all/AllChannelsViewModel.kt
@@ -2,26 +2,48 @@ package com.elmirov.course.presentation.channels.all
 
 import androidx.lifecycle.ViewModel
 import com.elmirov.course.domain.Channel
+import com.elmirov.course.domain.Topic
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class AllChannelsViewModel: ViewModel() {
+class AllChannelsViewModel : ViewModel() {
 
-    private val testData = listOf(
+    private val testData = mutableListOf(
         Channel(
-            id = 1,
+            id = 0,
             name = "#general all"
         ),
         Channel(
-            id = 2,
-            name = "#computing all"
+            id = 1,
+            name = "#computing all",
         ),
         Channel(
-            id = 3,
+            id = 2,
             name = "#PR all"
         ),
     )
 
-    private val _allChannels = MutableStateFlow(AllChannelsState.Content(testData))
+    private val topics = listOf(
+        Topic(
+            "первый all",
+            12,
+        ),
+        Topic(
+            "другой all",
+            1,
+        ),
+        Topic(
+            "last all",
+            123,
+        ),
+    )
+
+    private val _allChannels = MutableStateFlow(AllChannelsState.Content(testData.toList()))
     val allChannels = _allChannels.asStateFlow()
+
+    fun showTopics(channelId: Int) {
+        val channelWithTopics = testData[channelId].copy(topics = topics)
+        testData[channelId] = channelWithTopics
+        _allChannels.value = AllChannelsState.Content(testData.toList())
+    }
 }
--- app/src/main/java/com/elmirov/course/presentation/channels/subscribed/SubscribedChannelsViewModel.kt
+++ app/src/main/java/com/elmirov/course/presentation/channels/subscribed/SubscribedChannelsViewModel.kt
@@ -2,26 +2,49 @@ package com.elmirov.course.presentation.channels.subscribed
 
 import androidx.lifecycle.ViewModel
 import com.elmirov.course.domain.Channel
+import com.elmirov.course.domain.Topic
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class SubscribedChannelsViewModel : ViewModel() {
 
-    private val testData = listOf(
+    private val testData = mutableListOf(
         Channel(
-            id = 1,
+            id = 0,
             name = "#general subscribed"
         ),
         Channel(
-            id = 2,
+            id = 1,
             name = "#computing subscribed"
         ),
         Channel(
-            id = 3,
+            id = 2,
             name = "#PR subscribed"
         ),
     )
 
-    private val _subscribedChannels = MutableStateFlow(SubscribedChannelsState.Content(testData))
+    private val topics = listOf(
+        Topic(
+            "первый subscribed",
+            12,
+        ),
+        Topic(
+            "другой subscribed",
+            1,
+        ),
+        Topic(
+            "last subscribed",
+            123,
+        ),
+    )
+
+    private val _subscribedChannels =
+        MutableStateFlow(SubscribedChannelsState.Content(testData.toList()))
     val subscribedChannels = _subscribedChannels.asStateFlow()
+
+    fun showTopics(channelId: Int) {
+        val channelWithTopics = testData[channelId].copy(topics = topics)
+        testData[channelId] = channelWithTopics
+        _subscribedChannels.value = SubscribedChannelsState.Content(testData.toList())
+    }
 }
