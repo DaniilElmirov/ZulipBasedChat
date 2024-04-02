42e2a6ac0240a1b1a670e67bfe66315964941645
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-02 19:34:24 +0700

    Поменял разметку и реализацию MainActivity

--- app/src/main/java/com/elmirov/course/ui/activity/MainActivity.kt
+++ app/src/main/java/com/elmirov/course/ui/activity/MainActivity.kt
@@ -3,9 +3,12 @@ package com.elmirov.course.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.elmirov.course.CourseApplication
+import com.elmirov.course.R
 import com.elmirov.course.databinding.ActivityMainBinding
+import com.elmirov.course.navigation.Screens
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.androidx.AppNavigator
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +21,26 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var navigatorHolder: NavigatorHolder
 
+    private val navigator = AppNavigator(this, R.id.fragment_container)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as CourseApplication).component.inject(this)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null)
+            router.newRootScreen(Screens.MainScreen())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navigatorHolder.removeNavigator()
     }
 }
