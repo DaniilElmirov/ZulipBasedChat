a83a2a8fec4d85fbb4ab6f4b67060c2b08ffd15e
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-09 14:27:24 +0700

    Переписал вью модель экрана контактов через юзкейс

--- app/src/main/java/com/elmirov/course/presentation/users/UsersViewModel.kt
+++ app/src/main/java/com/elmirov/course/presentation/users/UsersViewModel.kt
@@ -2,100 +2,18 @@ package com.elmirov.course.presentation.users
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.elmirov.course.domain.entity.User
+import com.elmirov.course.domain.entity.Result
+import com.elmirov.course.domain.usecase.GetUsersUseCase
 import com.elmirov.course.navigation.router.GlobalRouter
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class UsersViewModel @Inject constructor(
-    private val router: GlobalRouter
+    private val getUsersUseCase: GetUsersUseCase,
+    private val router: GlobalRouter,
 ) : ViewModel() {
-
-    private val testData = listOf(
-        User(
-            id = 1,
-            name = "Имя фамилия 1",
-            mail = "почта1@gmail.com"
-        ),
-        User(
-            id = 2,
-            name = "Имя фамилия 2",
-            mail = "почта2@gmail.com"
-        ),
-        User(
-            id = 3,
-            name = "Имя фамилия 3",
-            mail = "почта3@gmail.com"
-        ),
-        User(
-            id = 4,
-            name = "Имя фамилия 4",
-            mail = "почта4@gmail.com"
-        ),
-        User(
-            id = 5,
-            name = "Имя фамилия 5",
-            mail = "почта5@gmail.com"
-        ),
-        User(
-            id = 6,
-            name = "Имя фамилия 6",
-            mail = "почта6@gmail.com"
-        ),
-        User(
-            id = 7,
-            name = "Имя фамилия 7",
-            mail = "почта7@gmail.com"
-        ),
-        User(
-            id = 8,
-            name = "Имя фамилия 8",
-            mail = "почта8@gmail.com"
-        ),
-        User(
-            id = 9,
-            name = "Имя фамилия 9",
-            mail = "почта9@gmail.com"
-        ),
-        User(
-            id = 10,
-            name = "Имя фамилия 10",
-            mail = "почта10@gmail.com"
-        ),
-        User(
-            id = 11,
-            name = "Имя фамилия 11",
-            mail = "почта11@gmail.com"
-        ),
-        User(
-            id = 12,
-            name = "Имя фамилия 12",
-            mail = "почта12@gmail.com"
-        ),
-        User(
-            id = 13,
-            name = "Имя фамилия 13",
-            mail = "почта13@gmail.com"
-        ),
-        User(
-            id = 14,
-            name = "Имя фамилия 14",
-            mail = "почта14@gmail.com"
-        ),
-        User(
-            id = 15,
-            name = "Имя фамилия 15",
-            mail = "почта15@gmail.com"
-        ),
-        User(
-            id = 16,
-            name = "Имя фамилия 16",
-            mail = "почта16@gmail.com"
-        ),
-    )
 
     private val _users = MutableStateFlow<UsersState>(UsersState.Loading)
     val users = _users.asStateFlow()
@@ -106,8 +24,10 @@ class UsersViewModel @Inject constructor(
 
     private fun loadUsers() {
         viewModelScope.launch {
-            delay(1000)
-            _users.value = UsersState.Content(testData)
+            when (val result = getUsersUseCase()) {
+                is Result.Error -> Unit
+                is Result.Success -> _users.value = UsersState.Content(result.data)
+            }
         }
     }
 
