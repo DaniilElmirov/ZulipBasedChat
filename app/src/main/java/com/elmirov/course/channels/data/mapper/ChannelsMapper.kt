ad9b7d31153540ca56275db2917070f97b2ac732
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-12 01:21:59 +0700

    Добавил реализацию репозитория для стримов с подпиской

--- app/src/main/java/com/elmirov/course/channels/data/mapper/ChannelsMapper.kt
+++ app/src/main/java/com/elmirov/course/channels/data/mapper/ChannelsMapper.kt
@@ -2,9 +2,14 @@ package com.elmirov.course.channels.data.mapper
 
 import com.elmirov.course.channels.data.model.AllChannelsResponseModel
 import com.elmirov.course.channels.data.model.ChannelModel
+import com.elmirov.course.channels.data.model.SubscribedChannelsResponseModel
 import com.elmirov.course.channels.domain.entity.Channel
 
 fun AllChannelsResponseModel.toEntities(): List<Channel> = channels.map {
+    it.toEntity()
+}
+
+fun SubscribedChannelsResponseModel.toEntities(): List<Channel> = channels.map {
     it.toEntity()
 }
 
---
+++ app/src/main/java/com/elmirov/course/channels/data/repository/SubscribedChannelsRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package com.elmirov.course.channels.data.repository
+
+import com.elmirov.course.channels.data.mapper.toEntities
+import com.elmirov.course.channels.data.network.SubscribedChannelsApi
+import com.elmirov.course.channels.domain.entity.Channel
+import com.elmirov.course.channels.domain.repository.SubscribedChannelsRepository
+import com.elmirov.course.core.di.annotation.DispatcherIo
+import com.elmirov.course.core.entity.ErrorType
+import com.elmirov.course.core.entity.Result
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+class SubscribedChannelsRepositoryImpl @Inject constructor(
+    private val api: SubscribedChannelsApi,
+    @DispatcherIo private val dispatcherIo: CoroutineDispatcher,
+) : SubscribedChannelsRepository {
+
+    override suspend fun get(): Result<List<Channel>> =
+        try {
+            withContext(dispatcherIo) {
+                Result.Success(api.get().toEntities())
+            }
+        } catch (cancellation: CancellationException) {
+            throw cancellation
+        } catch (exception: Exception) {
+            when (exception) {
+                is UnknownHostException, is SocketTimeoutException, is ConnectException -> {
+                    Result.Error(errorType = ErrorType.CONNECTION)
+                }
+
+                else -> {
+                    Result.Error(errorType = ErrorType.UNKNOWN)
+                }
+            }
+        }
+}
