63e24da96da28c0ffca09c44e6273ae92951b471
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-12 00:52:29 +0700

    Добавил маппер для стримов

---
+++ app/src/main/java/com/elmirov/course/channels/data/mapper/ChannelsMapper.kt
@@ -0,0 +1,15 @@
+package com.elmirov.course.channels.data.mapper
+
+import com.elmirov.course.channels.data.model.AllChannelsResponseModel
+import com.elmirov.course.channels.data.model.ChannelModel
+import com.elmirov.course.channels.domain.entity.Channel
+
+fun AllChannelsResponseModel.toEntities(): List<Channel> = channels.map {
+    it.toEntity()
+}
+
+private fun ChannelModel.toEntity(): Channel =
+    Channel(
+        id = this.id,
+        name = this.name,
+    )
