5c68b357c0c68268bfba9f00ba23465bd2b376f2
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-12 00:55:21 +0700

    Добавил работу с сетью в di модуль

--- app/src/main/java/com/elmirov/course/core/di/module/DataModule.kt
+++ app/src/main/java/com/elmirov/course/core/di/module/DataModule.kt
@@ -1,15 +1,64 @@
 package com.elmirov.course.core.di.module
 
-import com.elmirov.course.users.data.repository.UsersRepositoryImpl
+import com.elmirov.course.channels.data.network.AllChannelsApi
+import com.elmirov.course.channels.data.repository.AllChannelsRepositoryImpl
+import com.elmirov.course.channels.domain.repository.AllChannelsRepository
 import com.elmirov.course.core.di.annotation.ApplicationScope
+import com.elmirov.course.core.network.AuthorizationInterceptor
+import com.elmirov.course.users.data.repository.UsersRepositoryImpl
 import com.elmirov.course.users.domain.repository.UsersRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+@Module(includes = [BindDataModule::class])
+class DataModule {
+
+    private companion object {
+
+        private const val BASE_URL = "https://tinkoff-android-spring-2024.zulipchat.com/api/v1/"
+    }
+
+    @Provides
+    @ApplicationScope
+    fun provideHttpClient(authorizationInterceptor: AuthorizationInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .addInterceptor(authorizationInterceptor)
+            .build()
+
+    @Provides
+    @ApplicationScope
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .client(client)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @ApplicationScope
+    fun provideAllChannelsApi(retrofit: Retrofit): AllChannelsApi =
+        retrofit.create()
+}
 
 @Module
-interface DataModule {
+interface BindDataModule {
 
     @Binds
     @ApplicationScope
     fun bindUsersRepository(impl: UsersRepositoryImpl): UsersRepository
+
+    @Binds
+    @ApplicationScope
+    fun bindAllChannelsRepository(impl: AllChannelsRepositoryImpl): AllChannelsRepository
 }
