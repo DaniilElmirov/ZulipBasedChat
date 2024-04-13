f107d07900867247704e319a491cb4848f7bf7e1
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-13 21:11:22 +0700

    Поменял сущность стрима
    Не уверен в правильности подхода

--- app/src/main/java/com/elmirov/course/channels/domain/entity/Channel.kt
+++ app/src/main/java/com/elmirov/course/channels/domain/entity/Channel.kt
@@ -3,5 +3,6 @@ package com.elmirov.course.channels.domain.entity
 data class Channel(
     val id: Int,
     val name: String,
-    val topics: List<Topic>? = null
+    val expanded: Boolean = false, //TODO возможно плохой подход в совокупности с методами vm
+    val topics: List<Topic>? = null,
 )
