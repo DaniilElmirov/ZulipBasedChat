b342f1c755dd4c072fb66d262b7108f7d90e09fa
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-13 15:19:27 +0700

    Добавил реализацию репозитория для профиля

---
+++ app/src/main/java/com/elmirov/course/profile/data/repository/OtherProfileRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package com.elmirov.course.profile.data.repository
+
+import com.elmirov.course.core.result.domain.entity.ErrorType
+import com.elmirov.course.core.result.domain.entity.Result
+import com.elmirov.course.core.user.domain.entity.User
+import com.elmirov.course.di.annotation.DispatcherIo
+import com.elmirov.course.profile.data.mapper.toEntity
+import com.elmirov.course.profile.data.network.ProfileApi
+import com.elmirov.course.profile.domain.repository.OtherProfileRepository
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+class OtherProfileRepositoryImpl @Inject constructor(
+    private val api: ProfileApi,
+    @DispatcherIo private val dispatcherIo: CoroutineDispatcher,
+) : OtherProfileRepository {
+
+    override suspend fun getById(id: Int): Result<User> =
+        try {
+            withContext(dispatcherIo) {
+                Result.Success(api.getOtherById(id).toEntity())
+            }
+        } catch (cancellation: CancellationException) {
+            throw cancellation
+        } catch (exception: Exception) {
+            when (exception) {
+                is UnknownHostException, is SocketTimeoutException, is ConnectException -> {
+                    Result.Error(errorType = ErrorType.CONNECTION)
+                }
+
+                else -> {
+                    Result.Error(errorType = ErrorType.UNKNOWN)
+                }
+            }
+        }
+}
