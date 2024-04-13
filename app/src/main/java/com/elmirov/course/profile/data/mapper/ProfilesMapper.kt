42d414a46df7effdca6b05c85ce38e67e2835b94
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-13 15:16:07 +0700

    Добавил маппер для профиля

--- app/src/main/java/com/elmirov/course/profile/data/mapper/ProfilesMapper.kt
+++ app/src/main/java/com/elmirov/course/profile/data/mapper/ProfilesMapper.kt
@@ -2,6 +2,9 @@ package com.elmirov.course.profile.data.mapper
 
 import com.elmirov.course.core.user.data.model.UserModel
 import com.elmirov.course.core.user.domain.entity.User
+import com.elmirov.course.profile.data.model.OtherProfileResponseModel
+
+fun OtherProfileResponseModel.toEntity(): User = userModel.toEntity()
 
 fun UserModel.toEntity(): User =
     User(
