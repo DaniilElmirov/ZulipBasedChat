5a2507d358a0fda55f667caebec084604f944d54
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-13 19:40:32 +0700

    Добавил интерфейс api для топиков

---
+++ app/src/main/java/com/elmirov/course/channels/data/model/TopicModel.kt
@@ -0,0 +1,8 @@
+package com.elmirov.course.channels.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class TopicModel(
+    val name: String,
+    @SerializedName("max_id") val lastMessageId: Int,
+)
