7ef6cf29cf387a4c7e3c36de65adcb9592d59f36
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-14 17:47:31 +0700

    Добавил маппер для сообщений

---
+++ app/src/main/java/com/elmirov/course/chat/data/mapper/MessagesMapper.kt
@@ -0,0 +1,16 @@
+package com.elmirov.course.chat.data.mapper
+
+import com.elmirov.course.chat.data.model.MessageModel
+import com.elmirov.course.chat.data.model.MessagesResponseModel
+import com.elmirov.course.chat.domain.entity.Message
+
+fun MessagesResponseModel.toEntities(): List<Message> = messageModels.map { it.toEntity() }
+
+private fun MessageModel.toEntity(): Message =
+    Message(
+        id = id,
+        myMessage = myMessage,
+        avatarUrl = avatarUrl,
+        authorName = authorName,
+        text = text,
+    )
