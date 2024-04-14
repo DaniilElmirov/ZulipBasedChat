f253360b9d6aefb75e81fa54d0dd0cc3d5d878c2
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-14 16:31:56 +0700

    Добавил юзкейс и репозиторий для получения сообщений

---
+++ app/src/main/java/com/elmirov/course/chat/domain/repository/MessagesRepository.kt
@@ -0,0 +1,12 @@
+package com.elmirov.course.chat.domain.repository
+
+import com.elmirov.course.chat.domain.entity.Message
+import com.elmirov.course.core.result.domain.entity.Result
+
+interface MessagesRepository {
+
+    suspend fun getChannelTopicMessages(
+        channelName: String,
+        topicName: String
+    ): Result<List<Message>>
+}
