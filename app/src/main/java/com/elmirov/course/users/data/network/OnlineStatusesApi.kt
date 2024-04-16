541ceeaa70f44edf2d8130af2711d47ae9089f9e
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-16 23:11:13 +0700

    Добавил метод получения онлайн статуса пользователя по id

--- app/src/main/java/com/elmirov/course/users/data/network/OnlineStatusesApi.kt
+++ app/src/main/java/com/elmirov/course/users/data/network/OnlineStatusesApi.kt
@@ -1,10 +1,17 @@
 package com.elmirov.course.users.data.network
 
 import com.elmirov.course.users.data.model.AllUsersOnlineStatusResponseModel
+import com.elmirov.course.users.data.model.UserOnlineStatusResponseModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface OnlineStatusesApi {
 
     @GET("realm/presence")
     suspend fun getAll(): AllUsersOnlineStatusResponseModel
+
+    @GET("users/{user_id_or_email}/presence")
+    suspend fun getById(
+        @Path("user_id_or_email") userId: Int
+    ): UserOnlineStatusResponseModel
 }
