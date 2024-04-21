8731a9eafa66bcfd1c3c4a0cb07bf99466dd90e6
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-21 17:06:19 +0700

    Добавил event для экрана профиля

---
+++ app/src/main/java/com/elmirov/course/profile/presentation/elm/ProfileEvent.kt
@@ -0,0 +1,18 @@
+package com.elmirov.course.profile.presentation.elm
+
+import com.elmirov.course.core.user.domain.entity.User
+
+sealed interface ProfileEvent {
+
+    sealed interface Ui : ProfileEvent {
+        data object Init : Ui
+
+        data object BackClick : Ui
+    }
+
+    sealed interface Internal : ProfileEvent {
+        data class ProfileLoadingSuccess(val data: User) : Internal
+
+        data object ProfileLoadingError : Internal
+    }
+}
