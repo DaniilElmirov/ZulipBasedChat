7e3907af838159b1aa9ae12099e8fef4c8ac457a
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-21 21:47:21 +0700

    Удалил неиспользуемую vm экрана пользователей

--- app/src/main/java/com/elmirov/course/di/module/ViewModelModule.kt
+++ app/src/main/java/com/elmirov/course/di/module/ViewModelModule.kt
@@ -5,18 +5,12 @@ import com.elmirov.course.channels.presentation.all.AllChannelsViewModel
 import com.elmirov.course.channels.presentation.subscribed.SubscribedChannelsViewModel
 import com.elmirov.course.chat.presentation.ChatViewModel
 import com.elmirov.course.di.annotation.ViewModelKey
-import com.elmirov.course.users.presentation.UsersViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
 @Module
 interface ViewModelModule {
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(UsersViewModel::class)
-    fun bindUsersViewModel(viewModel: UsersViewModel): ViewModel
 
     @Binds
     @IntoMap
