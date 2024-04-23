13f487f607aa10b692387504c616a32fad8d0ca4
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-23 18:18:47 +0700

    Добавил reducer для экрана стримов

---
+++ app/src/main/java/com/elmirov/course/channels/presentation/ChannelsReducer.kt
@@ -0,0 +1,91 @@
+package com.elmirov.course.channels.presentation
+
+import com.elmirov.course.navigation.router.GlobalRouter
+import vivid.money.elmslie.core.store.dsl.ScreenDslReducer
+import javax.inject.Inject
+
+class ChannelsReducer @Inject constructor(
+    private val globalRouter: GlobalRouter,
+) : ScreenDslReducer<
+        ChannelsEvent,
+        ChannelsEvent.Ui,
+        ChannelsEvent.Internal,
+        ChannelsState,
+        ChannelsEffect,
+        ChannelsCommand>(
+    ChannelsEvent.Ui::class, ChannelsEvent.Internal::class
+) {
+
+    private companion object {
+        const val EMPTY_CHANNEL_NAME = ""
+    }
+
+    override fun Result.internal(event: ChannelsEvent.Internal): Any = when (event) {
+        ChannelsEvent.Internal.LoadingError -> {
+            state { copy(loading = false) }
+            effects { +ChannelsEffect.ShowError }
+        }
+
+        is ChannelsEvent.Internal.ChannelsLoadingSuccess -> {
+            state { copy(loading = false, content = event.data) }
+        }
+
+        is ChannelsEvent.Internal.TopicsOpened -> {
+            val topics = event.data
+            val currentChannels = (state.content ?: emptyList()).toMutableList()
+
+            val targetId = event.data.getOrNull(0)?.topicChannelId ?: -1 //TODO переделать
+            val targetChannelIndex = currentChannels.indexOfFirst {
+                it.id == targetId
+            }
+
+            currentChannels[targetChannelIndex] =
+                currentChannels[targetChannelIndex].copy(expanded = true, topics = topics)
+
+            state { copy(loading = false, content = currentChannels.toList()) }
+        }
+
+        is ChannelsEvent.Internal.TopicsClosed -> {
+            val currentChannels = (state.content ?: emptyList()).toMutableList()
+            val targetChannelIndex = currentChannels.indexOfFirst {
+                it.id == event.channelId
+            }
+
+            currentChannels[targetChannelIndex] =
+                currentChannels[targetChannelIndex].copy(expanded = false, topics = null)
+
+            state { copy(loading = false, content = currentChannels.toList()) }
+        }
+    }
+
+    override fun Result.ui(event: ChannelsEvent.Ui): Any = when (event) {
+
+        ChannelsEvent.Ui.InitAll -> {
+            state { copy(loading = true) }
+            commands { +ChannelsCommand.LoadAll }
+        }
+
+        is ChannelsEvent.Ui.InitSubscribed -> {
+            state { copy(loading = true) }
+            commands { +ChannelsCommand.LoadSubscribed }
+        }
+
+        is ChannelsEvent.Ui.OnChannelClick -> {
+            val targetChannel = state.content?.find {
+                it.id == event.channelId
+            }
+
+            if (targetChannel?.expanded == true)
+                commands { +ChannelsCommand.CloseTopics(event.channelId) }
+            else
+                commands { +ChannelsCommand.OpenTopics(event.channelId) }
+        }
+
+        is ChannelsEvent.Ui.OnTopicClick -> {
+            val channel = state.content?.find { it.id == event.channelId }
+            val channelName = channel?.name ?: EMPTY_CHANNEL_NAME
+
+            globalRouter.openChat(channelName, event.topicName)
+        }
+    }
+}
