82b96c421e0940d9e3b86d266879e66260d3ef1f
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-23 18:01:15 +0700

    Добавил command для экрана стримов

---
+++ app/src/main/java/com/elmirov/course/channels/presentation/ChannelsCommand.kt
@@ -0,0 +1,11 @@
+package com.elmirov.course.channels.presentation
+
+sealed interface ChannelsCommand {
+    data object LoadAll : ChannelsCommand
+
+    data object LoadSubscribed : ChannelsCommand
+
+    data class OpenTopics(val channelId: Int) : ChannelsCommand
+
+    data class CloseTopics(val channelId: Int) : ChannelsCommand
+}
