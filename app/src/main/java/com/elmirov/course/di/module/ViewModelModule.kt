ee1a61dfb25fb8f21aab9551c32806b41cbda0d4
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-23 18:30:47 +0700

    Удалил неиспользуемую vm экрана всех стримов

--- app/src/main/java/com/elmirov/course/di/module/ViewModelModule.kt
+++ app/src/main/java/com/elmirov/course/di/module/ViewModelModule.kt
@@ -1,7 +1,6 @@
 package com.elmirov.course.di.module
 
 import androidx.lifecycle.ViewModel
-import com.elmirov.course.channels.presentation.all.AllChannelsViewModel
 import com.elmirov.course.channels.presentation.subscribed.SubscribedChannelsViewModel
 import com.elmirov.course.di.annotation.ViewModelKey
 import dagger.Binds
@@ -10,11 +9,6 @@ import dagger.multibindings.IntoMap
 
 @Module
 interface ViewModelModule {
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(AllChannelsViewModel::class)
-    fun bindAllChannelsViewModel(viewModel: AllChannelsViewModel): ViewModel
 
     @Binds
     @IntoMap
