8909943332af939534435245defdc5cb3778e5a5
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-29 21:22:52 +0700

    Пофиксил расчет состояния "выбрано" у реакции.

--- app/src/main/java/com/elmirov/course/chat/data/mapper/MessagesMapper.kt
+++ app/src/main/java/com/elmirov/course/chat/data/mapper/MessagesMapper.kt
@@ -20,21 +20,39 @@ private fun MessageModel.toEntity(): Message =
         reactions = reactionModels.toEntity(),
     )
 
-private fun List<ReactionModel>.toEntity(): Map<Reaction, ReactionParams> =
-    associate { reactionModel ->
-        val ownId = 709286 //TODO убрать в локальное хранилище
+private fun List<ReactionModel>.toEntity(): Map<Reaction, ReactionParams> {
+    val result = mutableMapOf<Reaction, ReactionParams>()
+
+    val ownId = 709286 //TODO убрать в локальное хранилище
+
+    forEach { reactionModel ->
         val code =
-            if (reactionModel.code.contains("-") || //TODO переделать
+            if (//TODO переделать
+                reactionModel.code.contains("-") ||
                 reactionModel.reactionType != ReactionModel.REACTION_TYPE_UNICODE_EMOJI
             )
                 ReactionModel.STUB_EMOJI_CODE
             else
                 reactionModel.code
-        Reaction(
+
+        val reaction = Reaction(
             emojiName = reactionModel.name,
             emojiCode = code,
-        ) to ReactionParams(
-            count = count { it.code == reactionModel.code },
-            selected = ownId == reactionModel.userId
         )
+
+        val selected =
+            if (result[reaction]?.selected == true)
+                true
+            else
+                ownId == reactionModel.userId
+
+        val params = ReactionParams(
+            count = count { it.code == reactionModel.code },
+            selected = selected
+        )
+
+        result[reaction] = params
     }
+
+    return result
+}
