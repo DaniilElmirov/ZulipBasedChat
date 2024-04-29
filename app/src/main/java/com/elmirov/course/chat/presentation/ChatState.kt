c84d0a5c017d13a12ca65439a3c6fed21873f50f
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-04-29 23:24:09 +0700

    Поменял стейт экрана чата.

--- app/src/main/java/com/elmirov/course/chat/presentation/ChatState.kt
+++ app/src/main/java/com/elmirov/course/chat/presentation/ChatState.kt
@@ -1,8 +1,10 @@
 package com.elmirov.course.chat.presentation
 
+import com.elmirov.course.chat.domain.entity.ChatInfo
 import com.elmirov.course.chat.domain.entity.Message
 
 data class ChatState(
     val loading: Boolean = false,
-    val content: List<Message>? = null
+    val content: List<Message>? = null,
+    val chatInfo: ChatInfo? = null,
 )
