fe61f450b6bfa19e66a42638e82af2dd031b2f48
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-03 18:53:23 +0700

    Добавил зависимости для room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,62 +1,68 @@
 plugins {
-	alias(libs.plugins.androidApplication)
-	alias(libs.plugins.jetbrainsKotlinAndroid)
-	alias(libs.plugins.ksp)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.ksp)
 }
 
 android {
-	namespace = "com.elmirov.course"
-	compileSdk = 34
+    namespace = "com.elmirov.course"
+    compileSdk = 34
 
-	defaultConfig {
-		applicationId = "com.elmirov.course"
-		minSdk = 26
-		targetSdk = 34
-		versionCode = 1
-		versionName = "1.0"
+    defaultConfig {
+        applicationId = "com.elmirov.course"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
 
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-	}
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
 
-	buildTypes {
-		release {
-			isMinifyEnabled = false
-			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-		}
-	}
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_1_8
-		targetCompatibility = JavaVersion.VERSION_1_8
-	}
-	kotlinOptions {
-		jvmTarget = "1.8"
-	}
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 
-	buildFeatures {
-		viewBinding = true
-	}
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.appcompat)
-	implementation(libs.material)
-	implementation(libs.androidx.activity)
-	implementation(libs.androidx.constraintlayout)
-	implementation(libs.lifecycle.runtime.ktx)
-	implementation (libs.androidx.fragment.ktx)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.androidx.fragment.ktx)
 
-	implementation(libs.dagger.core)
-	ksp(libs.dagger.compiler)
+    implementation(libs.dagger.core)
+    ksp(libs.dagger.compiler)
 
-	implementation(libs.cicerone)
+    implementation(libs.cicerone)
 
-	implementation(libs.shimmer)
+    implementation(libs.shimmer)
 
-	implementation(libs.coil)
+    implementation(libs.coil)
 
-	implementation(libs.bundles.retrofit)
+    implementation(libs.bundles.retrofit)
 
-	implementation (libs.bundles.elmslie)
+    implementation(libs.bundles.elmslie)
+
+    implementation(libs.bundles.room)
+    ksp(libs.room.compiler)
 }
