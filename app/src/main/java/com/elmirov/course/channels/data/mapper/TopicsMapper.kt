cf93c5275a277b979698de9069455c64f27b3a3d
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-04 15:38:25 +0700

    Добавил методы маппинга топиков

--- app/src/main/java/com/elmirov/course/channels/data/mapper/TopicsMapper.kt
+++ app/src/main/java/com/elmirov/course/channels/data/mapper/TopicsMapper.kt
@@ -1,5 +1,6 @@
 package com.elmirov.course.channels.data.mapper
 
+import com.elmirov.course.channels.data.local.model.TopicDbModel
 import com.elmirov.course.channels.data.remote.model.TopicModel
 import com.elmirov.course.channels.data.remote.model.TopicsResponseModel
 import com.elmirov.course.channels.domain.entity.Topic
@@ -12,4 +13,22 @@ private fun TopicModel.toEntity(topicChannelId: Int): Topic =
         topicChannelId = topicChannelId,
         name = name,
         messageCount = lastMessageId, //TODO временно, посмотреть api сообщений
+    )
+
+fun TopicsResponseModel.toDb(topicChannelId: Int): List<TopicDbModel> =
+    topicModels.map { it.toDb(topicChannelId) }
+
+fun List<TopicDbModel>.toEntities(): List<Topic> = map { it.toEntity() }
+
+private fun TopicDbModel.toEntity(): Topic =
+    Topic(
+        topicChannelId = channelId,
+        name = name,
+        messageCount = 0,
+    )
+
+private fun TopicModel.toDb(topicChannelId: Int): TopicDbModel =
+    TopicDbModel(
+        channelId = topicChannelId,
+        name = name,
     )
