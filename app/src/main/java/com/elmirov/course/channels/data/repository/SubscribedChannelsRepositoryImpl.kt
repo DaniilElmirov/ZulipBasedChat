aa69def918798affffdbc5d97aca746acb39bfb5
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-04 12:39:29 +0700

    Изменил реализацию получения списка стримов с подпиской

--- app/src/main/java/com/elmirov/course/channels/data/repository/SubscribedChannelsRepositoryImpl.kt
+++ app/src/main/java/com/elmirov/course/channels/data/repository/SubscribedChannelsRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.elmirov.course.channels.data.repository
 
+import com.elmirov.course.channels.data.local.dao.ChannelsDao
+import com.elmirov.course.channels.data.mapper.toDb
 import com.elmirov.course.channels.data.mapper.toEntities
 import com.elmirov.course.channels.data.remote.network.SubscribedChannelsApi
 import com.elmirov.course.channels.domain.entity.Channel
@@ -12,13 +14,17 @@ import javax.inject.Inject
 
 class SubscribedChannelsRepositoryImpl @Inject constructor(
     private val api: SubscribedChannelsApi,
+    private val dao: ChannelsDao,
     @DispatcherIo private val dispatcherIo: CoroutineDispatcher,
 ) : SubscribedChannelsRepository {
 
     override suspend fun get(): Result<List<Channel>> = getResultWithHandleError(
         dispatcher = dispatcherIo,
         data = {
-            api.get().toEntities()
+            val remoteData = api.get()
+            dao.insertSubscribed(remoteData.toDb())
+
+            dao.getSubscribed().toEntities()
         },
     )
 }
