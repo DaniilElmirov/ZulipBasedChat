c83d9968b3bea6f2d8417af2b587800a50bd507c
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-04 12:39:55 +0700

    Изменил сущность стрима БД

--- app/src/main/java/com/elmirov/course/channels/data/local/model/ChannelDbModel.kt
+++ app/src/main/java/com/elmirov/course/channels/data/local/model/ChannelDbModel.kt
@@ -1,16 +1,22 @@
 package com.elmirov.course.channels.data.local.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.elmirov.course.channels.data.local.model.ChannelDbModel.Companion.COLUMN_CHANNEL_ID
+import com.elmirov.course.channels.data.local.model.ChannelDbModel.Companion.COLUMN_SUBSCRIBED
 
-@Entity
+@Entity(indices = [Index(value = [COLUMN_CHANNEL_ID, COLUMN_SUBSCRIBED], unique = true)])
 data class ChannelDbModel(
     @PrimaryKey(autoGenerate = true) val id: Int = DEFAULT_ID,
-    val channelId: Int,
+    @ColumnInfo(name = COLUMN_CHANNEL_ID) val channelId: Int,
     val name: String,
-    val subscribed: Boolean,
+    @ColumnInfo(name = COLUMN_SUBSCRIBED) val subscribed: Boolean,
 ) {
     companion object {
         private const val DEFAULT_ID = 0
+        const val COLUMN_CHANNEL_ID = "channelId"
+        const val COLUMN_SUBSCRIBED = "subscribed"
     }
 }
