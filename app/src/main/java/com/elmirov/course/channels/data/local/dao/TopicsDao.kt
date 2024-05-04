439931cd6ec3550c18c3c267a3bb3ebcf978529d
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-04 15:38:04 +0700

    Добавил методы для топиков в dao

--- app/src/main/java/com/elmirov/course/channels/data/local/dao/TopicsDao.kt
+++ app/src/main/java/com/elmirov/course/channels/data/local/dao/TopicsDao.kt
@@ -1,7 +1,20 @@
 package com.elmirov.course.channels.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.elmirov.course.channels.data.local.model.TopicDbModel
 
 @Dao
 interface TopicsDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(topics: List<TopicDbModel>)
+
+    @Query("SELECT * FROM TopicDbModel WHERE channelId = :channelId")
+    suspend fun get(channelId: Int): List<TopicDbModel>
+
+    @Query("DELETE FROM TopicDbModel WHERE channelId = :channelId")
+    suspend fun delete(channelId: Int)
 }
