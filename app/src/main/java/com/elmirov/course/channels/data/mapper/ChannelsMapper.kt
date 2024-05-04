f92d4e0279ccf56849ee72f05c57da63005d47d3
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-04 21:53:31 +0700

    Refactor. Убрал неиспользуемые методы из мапперов.

--- app/src/main/java/com/elmirov/course/channels/data/mapper/ChannelsMapper.kt
+++ app/src/main/java/com/elmirov/course/channels/data/mapper/ChannelsMapper.kt
@@ -6,20 +6,6 @@ import com.elmirov.course.channels.data.remote.model.ChannelModel
 import com.elmirov.course.channels.data.remote.model.SubscribedChannelsResponseModel
 import com.elmirov.course.channels.domain.entity.Channel
 
-fun AllChannelsResponseModel.toEntities(): List<Channel> = channels.map {
-    it.toEntity()
-}
-
-fun SubscribedChannelsResponseModel.toEntities(): List<Channel> = channels.map {
-    it.toEntity()
-}
-
-private fun ChannelModel.toEntity(): Channel =
-    Channel(
-        id = this.id,
-        name = this.name,
-    )
-
 fun AllChannelsResponseModel.toDb(): List<ChannelDbModel> = channels.map { it.toDb(false) }
 
 fun SubscribedChannelsResponseModel.toDb(): List<ChannelDbModel> = channels.map { it.toDb(true) }
--- app/src/main/java/com/elmirov/course/channels/data/mapper/TopicsMapper.kt
+++ app/src/main/java/com/elmirov/course/channels/data/mapper/TopicsMapper.kt
@@ -5,16 +5,6 @@ import com.elmirov.course.channels.data.remote.model.TopicModel
 import com.elmirov.course.channels.data.remote.model.TopicsResponseModel
 import com.elmirov.course.channels.domain.entity.Topic
 
-fun TopicsResponseModel.toEntities(topicChannelId: Int): List<Topic> =
-    topicModels.map { it.toEntity(topicChannelId) }
-
-private fun TopicModel.toEntity(topicChannelId: Int): Topic =
-    Topic(
-        topicChannelId = topicChannelId,
-        name = name,
-        messageCount = lastMessageId, //TODO временно, посмотреть api сообщений
-    )
-
 fun TopicsResponseModel.toDb(topicChannelId: Int): List<TopicDbModel> =
     topicModels.map { it.toDb(topicChannelId) }
 
