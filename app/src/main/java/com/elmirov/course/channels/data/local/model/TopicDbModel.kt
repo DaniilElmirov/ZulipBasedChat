3658939084df93c88834b5f7f4ea280966b7164b
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-04 15:37:37 +0700

    Поменял сущность топика БД

--- app/src/main/java/com/elmirov/course/channels/data/local/model/TopicDbModel.kt
+++ app/src/main/java/com/elmirov/course/channels/data/local/model/TopicDbModel.kt
@@ -5,7 +5,11 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class TopicDbModel(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = DEFAULT_ID,
     val channelId: Int,
     val name: String,
-)
+) {
+    companion object {
+        private const val DEFAULT_ID = 0
+    }
+}
