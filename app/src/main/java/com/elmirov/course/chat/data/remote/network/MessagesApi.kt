dc88081b945114d94547a40fd96ed1fc38f86835
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-05 20:06:10 +0700

    Добавил методы подзагрузки сообщений в интерфейс api.

--- app/src/main/java/com/elmirov/course/chat/data/remote/network/MessagesApi.kt
+++ app/src/main/java/com/elmirov/course/chat/data/remote/network/MessagesApi.kt
@@ -12,6 +12,12 @@ interface MessagesApi {
         const val MAX_NUM_BEFORE = 10
         const val MAX_NUM_AFTER = 10
 
+        const val NEXT_NUM_BEFORE = 0
+        const val NEXT_NUM_AFTER = 20
+
+        const val PREV_NUM_BEFORE = 20
+        const val PREV_NUM_AFTER = 0
+
         const val ANCHOR_FIRST_UNREAD = "first_unread"
 
         const val TYPE_STREAM = "stream"
@@ -23,6 +29,24 @@ interface MessagesApi {
         @Query("anchor") anchor: String = ANCHOR_FIRST_UNREAD,
         @Query("num_before") numBefore: Int = MAX_NUM_BEFORE,
         @Query("num_after") numAfter: Int = MAX_NUM_AFTER,
+        @Query("apply_markdown") applyMarkdown: Boolean = false,
+    ): MessagesResponseModel
+
+    @GET("messages")
+    suspend fun getNextMessages(
+        @Query("narrow") narrow: String,
+        @Query("anchor") anchor: String,
+        @Query("num_before") numBefore: Int = NEXT_NUM_BEFORE,
+        @Query("num_after") numAfter: Int = NEXT_NUM_AFTER,
+        @Query("apply_markdown") applyMarkdown: Boolean = false,
+    ): MessagesResponseModel
+
+    @GET("messages")
+    suspend fun getPrevMessages(
+        @Query("narrow") narrow: String,
+        @Query("anchor") anchor: String,
+        @Query("num_before") numBefore: Int = PREV_NUM_BEFORE,
+        @Query("num_after") numAfter: Int = PREV_NUM_AFTER,
         @Query("apply_markdown") applyMarkdown: Boolean = false,
     ): MessagesResponseModel
 
