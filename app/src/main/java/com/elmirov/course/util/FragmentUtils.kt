d32d99a9baf464e0b484235a2e27d0b4675f2815
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-05-25 17:43:36 +0700

    Добавил метод для показа диалога

--- app/src/main/java/com/elmirov/course/util/FragmentUtils.kt
+++ app/src/main/java/com/elmirov/course/util/FragmentUtils.kt
@@ -1,8 +1,11 @@
 package com.elmirov.course.util
 
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
+
+private const val EMPTY_STRING = ""
 
 fun Fragment.getErrorSnackBar(
     textResId: Int,
@@ -14,4 +17,34 @@ fun Fragment.getErrorSnackBar(
     snack.setAction(actionText, actionListener)
 
     return snack
+}
+
+fun Fragment.showDialog(
+    title: String = EMPTY_STRING,
+    layout: View? = null,
+    message: String? = null,
+    positiveButtonText: String = EMPTY_STRING,
+    onPositiveButtonClick: () -> Unit,
+    negativeButtonText: String = EMPTY_STRING,
+    onNegativeButtonClick: (() -> Unit)? = null,
+) {
+    AlertDialog.Builder(requireContext()).apply {
+        setTitle(title)
+
+        if (layout != null)
+            setView(layout)
+
+        setMessage(message)
+
+        setPositiveButton(positiveButtonText) { _, _ ->
+            onPositiveButtonClick()
+        }
+
+        setNegativeButton(negativeButtonText) { _, _ ->
+            onNegativeButtonClick?.let { it() }
+        }
+
+        create()
+        show()
+    }
 }
